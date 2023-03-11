0609581de6b261999933586fe8264e17034564a4
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-11 22:45:52 +0100

    Create class Main

--- src/pets/Main.java
+++ src/pets/Main.java
@@ -1,5 +1,11 @@
 package pets;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 // Создать класс Pet (домашнее животное). В классе должны быть:
@@ -16,7 +22,19 @@ public class Main {
 // Формат входных данных: количество строк с записями о домашних животных,
 // затем сами записи в описанном формате.
 
-  public static void main(String[] args) {
-
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    List<Pet> pets = new ArrayList<>(n);
+    for (int i = 0; i < n; ++i) {
+      String line = br.readLine();
+      Pet pet = Pet.parsePet(line);
+      pets.add(pet);
+    }
+    for (Pet pet : pets) {
+          System.out.printf("%-6s %-10s %8.3f %s\n", pet.getKind(), pet.getNickname(), pet.getWeight(),
+          pet.getDataBirthday());
+    }
   }
 }
+
