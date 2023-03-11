0e9a6187abe703428b4751b60063557d77f5f222
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-11 09:27:04 +0100

    Add constructors

--- src/pets/Pet.java
+++ src/pets/Pet.java
@@ -1,7 +1,7 @@
 package pets;
 
 public class Pet {
-  private enum Kind{
+  private enum Kind {
     DOG,
     CAT,
     OTHER
@@ -12,4 +12,34 @@ public class Pet {
   private String dataBirthday;
   private Double weight;
 
+  Pet(String kind, String nickname) {
+    setKind(kind);
+    this.nickname = nickname;
+  }
+
+  Pet(String kind, String nickname, Double weight) {
+    setKind(kind);
+    this.nickname = nickname;
+    this.weight = weight;
+  }
+
+  Pet(String kind, String nickname, String dataBirthday, Double weight) {
+    setKind(kind);
+    this.nickname = nickname;
+    this.dataBirthday = dataBirthday;
+    this.weight = weight;
+  }
+
+  private void setKind(String kind) {
+    if (kind.equalsIgnoreCase("dog")) {
+      this.kind = Kind.DOG;
+    } else if (kind.equalsIgnoreCase("cat")) {
+      this.kind = Kind.CAT;
+    } else {
+      this.kind = Kind.OTHER;
+    }
+  }
+
 }
+
+
