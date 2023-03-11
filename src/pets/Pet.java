7b2e62a41a273236b1d7e0eb18f098799a4ec9de
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-11 09:16:11 +0100

    Add fields

--- src/pets/Pet.java
+++ src/pets/Pet.java
@@ -1,5 +1,15 @@
 package pets;
 
 public class Pet {
+  private enum Kind{
+    DOG,
+    CAT,
+    OTHER
+  }
+
+  private Pet.Kind kind;
+  private String nickname;
+  private String dataBirthday;
+  private Double weight;
 
 }
